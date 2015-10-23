8e9c4bc115aa9c96f764a9ca1ae86311c58a8cb3
Lubos Matl <matllubos@gmail.com>
2015-10-23 14:27:06 +0200

    v1.0.4 Removed pk from email template

--- mailer/email_templates/models.py
+++ mailer/email_templates/models.py
@@ -8,7 +8,7 @@ from easymode.i18n.decorators import I18n
 
 @I18n('subject', 'html_body')
 class AbstractEmailTemplate(models.Model):
-    slug = models.SlugField(max_length=100, null=False, blank=False, unique=True, verbose_name=_('Slug'))
+    slug = models.SlugField(max_length=100, null=False, blank=False, primary_key=True, verbose_name=_('Slug'))
     subject = models.CharField(max_length=100, null=True, blank=False, verbose_name=_('Subject'))
     html_body = models.TextField(null=True, blank=False, verbose_name=_('HTML body'))
 
